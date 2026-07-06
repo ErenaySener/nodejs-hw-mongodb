0c7bb48bf20e3eac729e8e6e32f8d7efb0b449f1
ErenaySener <erenaysener@hotmail.com>
2026-07-06 12:34:39 +0200

    Add contact by id

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,4 +1,7 @@
-import { getAllContacts } from '../services/contacts.js';
+import {
+  getAllContacts,
+  getContactById,
+} from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
   const contacts = await getAllContacts();
@@ -7,5 +10,23 @@ export const getContactsController = async (req, res) => {
     status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    return res.status(404).json({
+      message: 'Contact not found',
+    });
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
   });
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -5,3 +5,9 @@ export const getAllContacts = async () => {
 
   return contacts;
 };
+
+export const getContactById = async (contactId) => {
+  const contact = await Contact.findById(contactId);
+
+  return contact;
+};
