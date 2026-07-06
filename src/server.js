6d616aed2c0c70cb382dba5834515144fd367c67
ErenaySener <erenaysener@hotmail.com>
2026-07-06 12:02:49 +0200

    Add contacts endpoint

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import contactsRouter from './routes/contacts.js';
 
 export const setupServer = () => {
   const app = express();
@@ -8,6 +9,8 @@ export const setupServer = () => {
   app.use(cors());
 
   app.use(pino());
+
+  app.use(contactsRouter);
 
   app.use((req, res) => {
     res.status(404).json({
---
+++ src/services/contacts.js
@@ -0,0 +1,7 @@
+import { Contact } from '../models/contact.js';
+
+export const getAllContacts = async () => {
+  const contacts = await Contact.find();
+
+  return contacts;
+};
