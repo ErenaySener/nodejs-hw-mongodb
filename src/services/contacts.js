d347041fe3795da7d8eb521406d829610511bc3e
ErenaySener <erenaysener@hotmail.com>
2026-07-13 14:36:39 +0200

    Add validation, pagination, sorting and filtering

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,34 @@
 import { Contact } from '../models/contact.js';
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
+export const getAllContacts = async (
+  page = 1,
+  perPage = 10,
+  sortBy = 'name',
+  sortOrder = 'asc',
+  filters = {},
+) => {
+  const skip = (page - 1) * perPage;
 
-  return contacts;
+  const contacts = await Contact.find(filters)
+    .skip(skip)
+    .limit(perPage)
+    .sort({
+      [sortBy]: sortOrder,
+    });
+
+  const totalItems = await Contact.countDocuments(filters);
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  return {
+    data: contacts,
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    hasPreviousPage: page > 1,
+    hasNextPage: page < totalPages,
+  };
 };
 
 export const getContactById = async (contactId) => {
